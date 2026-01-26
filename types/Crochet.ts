b92cd69d6108107df8176efd6331fa5c3a42d6fa
codecanna <codecannamw@gmail.com>
2026-01-26 00:03:43 -0700

    added crochetType field

--- types/Crochet.ts
+++ types/Crochet.ts
@@ -1,8 +1,10 @@
+import { CrochetType } from "../enums/CrochetType.ts";
 import { Difficulty } from "../enums/Difficulty.ts";
 import { CrochetStitch } from "../enums/Stitches.ts";
 
 export type Crochet = {
     hookSize: number,
     stitches: CrochetStitch[],
-    difficulty: Difficulty
+    difficulty: Difficulty,
+    crochetType: CrochetType[]
 }
