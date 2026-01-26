c46964f3b2bc3e139a66b33ac9d1a2d6148f936f
codecanna <codecannamw@gmail.com>
2026-01-26 00:02:58 -0700

    added more tags

--- enums/Tags.ts
+++ enums/Tags.ts
@@ -1,19 +1,42 @@
 export enum Tags {
+  // Wearables
   Clothing = "clothing",
+  Sweater = "sweater",
+  Cardigan = "cardigan",
+  Vest = "vest",
   Hat = "hat",
-  Scarf = "scarf",
-  Amigurumi = "amigurumi",
-  Gloves = "gloves",
-  Socks = "socks",
-  Keychain = "keychain",
   Beanie = "beanie",
-  Pillow = "pillow",
-  Blanket = "blanket",
+  Scarf = "scarf",
+  Cowl = "cowl",
+  Shawl = "shawl",
+  Gloves = "gloves",
+  Mittens = "mittens",
+  Socks = "socks",
+  Headband = "headband",
+  
+  // Bags & Accessories
   Bag = "bag",
-  Coaster = "coaster",
-  Drink_Accessory = "drink_accessory",
-  Pet_Clothes = "pet_clothes",
-  Cushon = "cushon",
   Purse = "purse",
+  Keychain = "keychain",
+  Jewelry = "jewelry",
+  
+  // Home & Kitchen
+  Pillow = "pillow",
+  Cushion = "cushion", // Fixed spelling
+  Blanket = "blanket",
+  Coaster = "coaster",
+  Dishcloth = "dishcloth",
+  Basket = "basket",
+  Wall_Hanging = "wall_hanging",
+  Drink_Accessory = "drink_accessory",
+  
+  // Toys & Others
+  Amigurumi = "amigurumi",
+  Toy = "toy",
+  Pet_Clothes = "pet_clothes",
+  Baby_Item = "baby_item",
+  Holiday = "holiday",
+  Applique = "applique",
+  
   Other = "other",
 }
