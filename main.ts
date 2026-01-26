8395fd6052f260fb15e20c23a1efcf2a312b4d6b
codecanna <codecannamw@gmail.com>
2026-01-26 00:01:38 -0700

    added conversations

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
-import { Bot } from "grammy";
+import { Bot, Context } from "grammy";
 import { downloadFile, fileType } from "./utils/misc.ts";
 import { print } from "./utils/print.ts";
+import {
+  type Conversation,
+  type ConversationFlavor,
+  conversations,
+  createConversation,
+} from "grammyjs/conversations";
+import { new_pattern } from "./handlers/new_pattern.ts";
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  const conn: RedisConnectOptions = {
-    hostname: "127.0.0.1",
-    port: 6379,
-  };
-  const PatternLibrarian = new Bot(Deno.env.get("TELEGRAM_BOT_KEY") || "");
+  type PatternLibrarianContext = Context & ConversationFlavor<Context>;
+  const PatternLibrarian = new Bot<PatternLibrarianContext>(
+    Deno.env.get("TELEGRAM_BOT_KEY") || "",
+  );
+  PatternLibrarian.use(conversations());
+  PatternLibrarian.use(createConversation(new_pattern));
 
   PatternLibrarian.command("start", async (ctx) => {
     await ctx.reply("Hello!");
@@ -19,9 +27,17 @@ if (import.meta.main) {
     if (file) {
       switch (fileType(file.file_path!)) {
         case "pdf": {
-          await ctx.reply("You sent a PDF file!");
-          const fileUrl = `https://api.telegram.org/file/bot${PatternLibrarian.token}/${file.file_path}`;
-          downloadFile(fileUrl, "./assets/pattern_files/pdf/");
+          const fileUrl =
+            `https://api.telegram.org/file/bot${PatternLibrarian.token}/${file.file_path}`;
+          await ctx.conversation.enter("new_pattern", fileUrl);
+          const patternFilePath = await downloadFile(
+            fileUrl,
+            `${ctx.from.id}.pdf`,
+          );
+          await Deno.copyFile(
+            patternFilePath,
+            "./assets/pattern_files/pdf/beans.pdf",
+          );
           break;
         }
         case "txt": {
@@ -29,7 +45,9 @@ if (import.meta.main) {
           break;
         }
         default: {
-          await ctx.reply("Invalid file type detected, I can only accept PDF or Text files.");
+          await ctx.reply(
+            "Invalid file type detected, I can only accept PDF or Text files.",
+          );
         }
       }
     }
