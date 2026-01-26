be9e34551de279764f734eb7c50b866ddbc708c9
codecanna <codecannamw@gmail.com>
2026-01-26 00:02:27 -0700

    added name field

---
+++ classes/Pattern.ts
@@ -0,0 +1,37 @@
+import { Tags } from "../enums/Tags.ts";
+import { Crochet } from "../types/Crochet.ts";
+import { Knit } from "../types/Knit.ts";
+import { print } from "../utils/print.ts";
+
+/**
+ * This class represents either a crochet pattern or a knit pattern
+ */
+export class Pattern {
+    id: number;
+    userId: number;
+    name: string;
+    craft: Crochet | Knit;
+    tags: Tags[];
+    patternFilePath: string;
+    constructor(
+        id: number,
+        userId: number,
+        name: string,
+        craft: Crochet | Knit,
+        tags: Tags[],
+        patternFilePath: string
+    ) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.craft = craft;
+        this.tags = tags;
+        this.patternFilePath = patternFilePath;
+    }
+
+    set() {
+      print(`Set ${this}`);
+    }
+
+    async delete() {}
+}
