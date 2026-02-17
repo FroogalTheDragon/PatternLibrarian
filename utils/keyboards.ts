60bc5d4695b8df10687e23dbb89f78df7b465d58
FroogalTheDragon <codecannamw@gmail.com>
2026-02-17 13:11:33 -0700

    Added a way to build keyboard crom arrays

---
+++ utils/keyboards.ts
@@ -0,0 +1,14 @@
+import { Keyboard } from "grammy";
+
+/**
+ * Create a custom keyboard from an array of values
+ * @param arr The array of values to construct the keyboard from
+ * @returns Keyboard
+ */
+export function keyboardFromArray(arr: Array<string | number>): Keyboard {
+    const keyboard = new Keyboard();
+    for (let i = 0; i < arr.length; i++) {
+        keyboard.text(arr[i].toString()).row();
+    }
+    return keyboard;
+}
