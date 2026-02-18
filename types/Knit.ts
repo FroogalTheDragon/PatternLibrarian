e48517a2d75103b49a3c02c4abc380c84c0c6cb1
FroogalTheDragon <codecannamw@gmail.com>
2026-02-18 00:31:30 -0700

    made needlesSizes a number []

--- types/Knit.ts
+++ types/Knit.ts
@@ -2,7 +2,7 @@ import { Difficulty } from "../enums/Difficulty.ts";
 import { KnitStitch } from "../enums/Stitches.ts";
 
 export type Knit = {
-    needle_size: number,
+    needleSizes: number[],
     stitches: KnitStitch[],
     difficulty: Difficulty
 };
