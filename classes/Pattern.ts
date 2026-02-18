02c4b5b4ed44d5be698f919923588feec68bd5ab
FroogalTheDragon <codecannamw@gmail.com>
2026-02-18 00:30:07 -0700

    added timestamp and set()

--- classes/Pattern.ts
+++ classes/Pattern.ts
@@ -1,7 +1,9 @@
+import { connect } from "https://deno.land/x/redis@v0.40.0/redis.ts";
 import { Tags } from "../enums/Tags.ts";
 import { Crochet } from "../types/Crochet.ts";
 import { Knit } from "../types/Knit.ts";
 import { print } from "../utils/print.ts";
+import { hostname } from "node:os";
 
 /**
  * This class represents either a crochet pattern or a knit pattern
@@ -9,6 +11,8 @@ import { print } from "../utils/print.ts";
 export class Pattern {
     id?: number;
     userId?: number;
+    timestamp?: number;
+    modifiedTimestamp?: number;
     name?: string;
     description?: string;
     craft?: Crochet | Knit;
@@ -17,6 +21,8 @@ export class Pattern {
     constructor(
         id?: number,
         userId?: number,
+        timestamp?: number,
+        modifiedTimestamp?: number,
         name?: string,
         description?: string,
         craft?: Crochet | Knit,
@@ -25,6 +31,8 @@ export class Pattern {
     ) {
         this.id = id;
         this.userId = userId;
+        this.timestamp = timestamp;
+        this.modifiedTimestamp = modifiedTimestamp;
         this.name = name;
         this.description = description;
         this.craft = craft;
@@ -32,8 +40,17 @@ export class Pattern {
         this.patternFilePath = patternFilePath;
     }
 
-    set() {
-      print(`Set ${this}`);
+    /**
+     * 
+     * @returns Promise<RedisReply>
+     */
+    async set() {
+      const redis = await connect({
+        hostname: "127.0.0.1",
+        port: 6379
+      });
+
+      return await redis.sendCommand(`JSON.SET`, [`${this.userId}:${this.timestamp}`, "$", JSON.stringify(this)]);
     }
 
     async delete() {}
